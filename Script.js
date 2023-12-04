bd00d11fad90dde4e556c99cb4aeef55f9b0c19a
CallMeSteve1 <115586058+CallMeSteve1@users.noreply.github.com>
2023-12-04 10:40:35 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,9 +3,9 @@ function checkAnswer() {
 	
 	if (selectedAnswer !== null) {
 		if (selectedAnswer.value === 'b') {
-			document.getElementById('feedback').innerHTML = 'Correct!';
+			document.getElementById('feedback').innerHTML = "I'm sorry you didn't enjoy it!";
 		} else {
-			document.getElementById('feedback').innerHTML = 'Wrong answer. Try again!';
+			document.getElementById('feedback').innerHTML = 'Thank you very much for enjoying my list!';
 		}
 	} else {
 		document.getElementById('feedback').innerHTML = 'Please select an answer.';
